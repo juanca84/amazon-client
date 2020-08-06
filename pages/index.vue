bb2e49bbf441b28cfedf4b450d0bbd732e1c98db
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2020-08-05 23:31:34 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
+    <div>
+      <Logo />
+      <h1 class="title">
+        client
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
