d4cf05eb41ec5d89e8c00f5bfc413d05106d3afe
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2020-08-08 00:26:47 -0400

    Adición de dinamismo a la pagina de producto

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -7,7 +7,7 @@
                 <ul class="a-unordered-list a-horizontal a-size-small">
                     <li>
                         <span class="a-list-item">
-                            <a href="#" class="a-link-normal a-color-tertiary">Category</a>
+                            <a href="#" class="a-link-normal a-color-tertiary">{{ product.category.type }}</a>
                         </span>
                     </li>
                     <li>
@@ -15,7 +15,7 @@
                     </li>
                     <li>
                         <span class="a-list-item">
-                            <a href="#" class="a-link-normal a-color-tertiary">Product Title</a>
+                            <a href="#" class="a-link-normal a-color-tertiary">{{ product.title }}</a>
                         </span>
                     </li>
                 </ul>
@@ -30,7 +30,7 @@
                             <!-- Image -->
                             <div class="imgBlock">
                                 <div class="eBooksimg">
-                                    <img src="/img/steve_jobs.jpg" alt="" class="img-fluid">
+                                    <img :src="product.photo" alt="" class="img-fluid">
                                 </div>
                             </div>
                             <!-- Follow Author -->
@@ -43,14 +43,14 @@
                                         <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
                                             <div class="smallAuthorImageContainer">
                                                 <a href="#">
-                                                    <img src="/img/steve_jobs.jpg" alt="" class="img-fluid">
+                                                    <img :src="product.owner.photo" alt="" class="img-fluid">
                                                 </a>
                                             </div>
                                         </div>
                                         <!-- Author's name -->
                                         <div class="col-xl-4 col-lg-3 col-md-3 col-sm-3 col-3">
                                             <div class="authorNameCol">
-                                                <a href="#">Walter Isaacson</a>
+                                                <a href="#">{{ product.owner.name }}</a>
                                             </div>
                                         </div>
                                         <!-- Author's Follow Button -->
@@ -77,14 +77,14 @@
                             <!-- Product Title -->
                             <div class="titleDiv">
                                 <h1 class="productTitle">
-                                    <span class="largeTitle">Harry Potter</span>
+                                    <span class="largeTitle">{{ product.title }}</span>
                                     <span class="smallTitle">Paperback</span>
                                 </h1>
                             </div>
                             <!-- Author's name -->
                             <div class="bylineinfo">
                                 by
-                                <a href="#" class="authorName">Walter Issacson
+                                <a href="#" class="authorName">{{ product.owner.name }}
                                     <i class="fa fa-chevrondown" style="font-size: 8px Important; color: #555 !important;"></i>
                                 </a> (Author)
                             </div>
@@ -171,7 +171,7 @@
                             </div>
                             <!-- Description -->
                             <div class="bookDescription">
-                                <div class="bookDescriptionInner">This book is awesome</div>
+                                <div class="bookDescriptionInner">{{ product.description }}</div>
                             </div>
                             <!-- Product specification -->
                             <div class="aboutEbooksFeature">
@@ -201,7 +201,7 @@
                                         <!-- Product Price -->
                                         <div class="float-right">
                                             <span class="a-size-medium a-color-price offer-price a-text-normal">
-                                                $39
+                                                ${{ product.price }}
                                             </span>
                                         </div>
                                     </div>
@@ -230,7 +230,7 @@
                                     <div class="a-section a-spacing-none">
                                         <span class="a-size-medium a-color-success">In Stock</span>
                                     </div>
-                                    <div class="a-section a-spacing-mini">Shipts from and sold by Amazon.com</div>
+                                    <div class="a-section a-spacing-mini">Ships from and sold by Amazon.com</div>
                                 </div>
 
                                 <div class="a-section">
@@ -286,7 +286,7 @@
                                             </form>
                                         </div>
                                         <div class="float-right">
-                                            <span class="a-color-base offer-price a-text-normal">$39</span>
+                                            <span class="a-color-base offer-price a-text-normal">${{ product.price }}</span>
                                         </div>
                                     </div>
                                 </div>
@@ -309,7 +309,7 @@
                                     <div class="authorContent">
                                         <div class="authorImageSingle">
                                             <a href="#">
-                                                <img src="/img/steve_jobs.jpg" alt="" class="img-fluid"/>
+                                                <img :src="product.owner.photo" alt="" class="img-fluid"/>
                                             </a>
                                         </div>
                                         <div class="authorFollow">
@@ -324,7 +324,7 @@
                                 <div class="col-md-10 col-sm-8 col-8 pl-0">
                                     <div class="mainContent">
                                         <h3>Biography</h3>
-                                        <div id="authorBio">My name is Walter White</div>
+                                        <div id="authorBio">{{ product.owner.about }}</div>
                                     </div>
                                 </div>
                             </div>
@@ -338,7 +338,17 @@
 
 <script>
     export default {
-        
+        async asyncData({ $axios, params }) {
+            try {
+                let response = await $axios.$get(`/api/products/${params.id}`);
+                console.log(response);
+                return {
+                    product: response.product
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        },
     }
 </script>
 
