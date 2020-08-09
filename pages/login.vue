afccddccda482e09c27921c15048ff48166c0602
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2020-08-09 13:14:45 -0400

    Adición de la funcionalidad del login

--- pages/login.vue
+++ pages/login.vue
@@ -12,12 +12,7 @@
                     <form class="mt-4">
                         <div class="a-box a-spacing-extra-large">
                             <div class="a-box-inner">
-                                <h1 class="a-spacing-small">Create account</h1>
-                                <!-- your name -->
-                                <div class="a-row a-spacing-base">
-                                    <label for="ap_customer_name" class="a-form-label">Your name</label>
-                                    <input type="text" id="ap_customer_name" v-model="name" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
-                                </div>
+                                <h1 class="a-spacing-small">Sign In</h1>
                                 <!-- your email -->
                                 <div class="a-row a-spacing-base">
                                     <label for="ap_customer_name" class="a-form-label">Email</label>
@@ -35,7 +30,7 @@
                                 <div class="a-row a-spacing-extra-large mb-4">
                                     <span class="a-button-primary">
                                         <span class="a-button-inner">
-                                            <span class="a-button-text" @click="onSignup">Create your Amazon account</span>    
+                                            <span class="a-button-text" @click="onLogin">Continue</span>    
                                         </span>
                                     </span>
                                     <div class="a-row a-spacing-top-medium a-size-small">
@@ -49,8 +44,8 @@
                                 <hr/>
                                 <div class="a-row">
                                     <b>
-                                        Already have an account?
-                                        <nuxt-link to="/login" class="a-link-emphasis">Sign in</nuxt-link>
+                                        Don't have an account?
+                                        <nuxt-link to="/signup" class="a-link-emphasis">Signup</nuxt-link>
                                     </b>
                                 </div>
                             </div>
@@ -69,30 +64,20 @@
         layout: "none",
         data(){
             return{
-                name: "",
                 email: "",
                 password: ""
             }
         },
         methods: {
-            async onSignup(){
+            async onLogin() {
                 try {
-                    let data = {
-                        name: this.name,
-                        email: this.email,
-                        password: this.password
-                    }
-                    let response = await this.$axios.$post("/api/auth/signup", data);
-                    console.log(response);
-                    if (response.success) {
-                        this.$auth.loginWith("local",{
-                            data: {
-                                email: this.email,
-                                password: this.password
-                            }
-                        });
-                        this.$router.push("/");
-                    }
+                    this.$auth.loginWith("local",{
+                        data: {
+                            email: this.email,
+                            password: this.password
+                        }
+                    });
+                    this.$router.push("/");
                 } catch (error) {
                     console.log(error)
                 }
