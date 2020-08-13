05d0b93bceb4b6c5696a760e34205bf814a15db0
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2020-08-12 21:33:48 -0400

    Adición del profile del usuario

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,9 +5,9 @@
                 <div class="row">
                     <div class="col-sm-2">
                         <div class="logo-area">
-                            <a href="">
+                            <nuxt-link to="/">
                                 <img src="/img/logo.png" alt="logo" class="img-fluid">
-                            </a>
+                            </nuxt-link>
                         </div>
                     </div>
                     <!-- Search bar-->
@@ -68,13 +68,13 @@
                             </a>
                             <span class="icp-nav-link-border"></span>
                             <template v-if="$auth.$state.loggedIn">
-                                <nuxt-link to='/register' class="nav-a nav-a-2" id="nav-link-accountList" tabindex="0">
+                                <nuxt-link to='/profile' class="nav-a nav-a-2" id="nav-link-accountList" tabindex="0">
                                     <span class="nav-line-1">Hello,</span>
                                     <span class="nav-line-2">{{ $auth.$state.user.name }}</span>
                                 </nuxt-link>
                             </template>
                             <template v-else>
-                                <nuxt-link to='/register' class="nav-a nav-a-2" id="nav-link-accountList" tabindex="0">
+                                <nuxt-link to='/signup' class="nav-a nav-a-2" id="nav-link-accountList" tabindex="0">
                                     <span class="nav-line-1">Hello, Sign in</span>
                                     <span class="nav-line-2">
                                         Account &amp; Lists
