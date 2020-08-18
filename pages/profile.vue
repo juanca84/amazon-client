005ef24985317fed8279f2ccd643a10ec15d1e5d
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2020-08-18 00:40:16 -0400

    Adici'on del logout y acciones para guardar reviews

--- pages/profile.vue
+++ pages/profile.vue
@@ -6,6 +6,7 @@
                <div class="col-sm-6">
                     <div class="a-spacing-top-medium"></div>
                     <h2>Profile Page</h2>
+                    <a href="#" @click="onLogout">Logout</a>
                     <form>
                         <!-- Name -->
                         <div class="a-spacing-top-medium">
@@ -67,6 +68,9 @@
                 } catch (error) {
                     console.log(error);
                 }
+            },
+            async onLogout() {
+                await this.$auth.logout()
             }
         }
     }
