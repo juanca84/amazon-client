1e8b806e900b75108426c7c674b91bcc5fe687b2
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2020-08-23 22:33:39 -0400

    resolviendo problema para mostrar las estrellas

--- pages/index.vue
+++ pages/index.vue
@@ -89,7 +89,7 @@
                           <div class="a-row a-spacing-mini">
                             <!-- Star Ratings -->
                             <no-ssr>
-                              <star-rating :rating="product.averagerRating" :show-rating="false" :glow="1" :border-width="1" :rounded-corners="true" :read-only="true" :star-size="18" :star-point="[23,2,14,17,0,19,10,34,7,50,23,43,38,50,36,34,46,19,31,17]"></star-rating>
+                              <star-rating :rating="product.averageRating" :show-rating="false" :glow="1" :border-width="1" :rounded-corners="true" :read-only="true" :star-size="18" :star-point="[23,2,14,17,0,19,10,34,7,50,23,43,38,50,36,34,46,19,31,17]"></star-rating>
                             </no-ssr>
                           </div>
                         </div>
--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -94,7 +94,7 @@
                             </div>
                             <div class="reviewGroup">
                                 <no-ssr>
-                                    <star-rating :rating="product.averagerRating" :show-rating="false" :glow="1" :border-width="1" :rounded-corners="true" :read-only="true" :star-size="18" :star-point="[23,2,14,17,0,19,10,34,7,50,23,43,38,50,36,34,46,19,31,17]"></star-rating>
+                                    <star-rating :rating="product.averageRating" :show-rating="false" :glow="1" :border-width="1" :rounded-corners="true" :read-only="true" :star-size="18" :star-point="[23,2,14,17,0,19,10,34,7,50,23,43,38,50,36,34,46,19,31,17]"></star-rating>
                                 </no-ssr>
                             </div>
                             <hr style="margin-top: 10px;">
